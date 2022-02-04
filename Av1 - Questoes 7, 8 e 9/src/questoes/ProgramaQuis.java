d0e233e8173ffc43631084b9c54562586f55c0fe
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-04 20:29:05 -0300

    project creation

--- Av1 - Questoes 7, 8 e 9/src/questoes/ProgramaQuis.java
+++ Av1 - Questoes 7, 8 e 9/src/questoes/ProgramaQuis.java
@@ -10,10 +10,17 @@ public class ProgramaQuis {
 	        String pergunta1 = "Qual destes passwords foi o mais usado na internet? ";
 	        System.out.println(pergunta1);
 
-	        System.out.println("[1] a1b2c3");
+	        System.out.println("\n[1] a1b2c3");
 	        System.out.println("[2] abcdef");
 	        System.out.println("[3] 123456");
-	        System.out.println("Digite sua resposta: ");
+	        
+	        System.out.println("\nDigite sua resposta: ");
 	        String resposta = scanner.nextLine();
+	        
+	        if (resposta.equals("3")) {
+	        	System.out.println("Reposta Correta! ");
+	        }else {
+	        	System.out.println("Reposta Errada !!!!");
+	        }
 }
 }

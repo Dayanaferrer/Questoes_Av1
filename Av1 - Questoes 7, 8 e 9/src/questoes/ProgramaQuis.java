6c39462350f471b82fa76ce62fae127ddd96d37a
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-04 20:18:32 -0300

    Starting Program

--- Av1 - Questoes 7, 8 e 9/src/questoes/ProgramaQuis.java
+++ Av1 - Questoes 7, 8 e 9/src/questoes/ProgramaQuis.java
@@ -1,10 +1,19 @@
 package questoes;
+import java.util.Scanner;
 
 public class ProgramaQuis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        String pergunta1 = "Qual destes passwords foi o mais usado na internet? ";
+	        System.out.println(pergunta1);
 
-	}
-
+	        System.out.println("[1] a1b2c3");
+	        System.out.println("[2] abcdef");
+	        System.out.println("[3] 123456");
+	        System.out.println("Digite sua resposta: ");
+	        String resposta = scanner.nextLine();
+}
 }

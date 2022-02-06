5cf1ef9571d9d133c84c5db753a4d4a481111017
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-02-06 14:26:54 -0300

    Testing the screen that interacts with the user

--- Av1 - Questoes 7, 8 e 9/src/questao8/LoginDoSistema.java
+++ Av1 - Questoes 7, 8 e 9/src/questao8/LoginDoSistema.java
@@ -1,4 +1,6 @@
 package questao8;
+import java.awt.Font;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -16,15 +18,32 @@ public class LoginDoSistema implements ActionListener{
 	JButton botaoDeReiniciar = new JButton("Recarregar");
 	JTextField nomeUsuario = new JTextField ();
 	JPasswordField senhaUsuario = new JPasswordField();
-	JLabel digiteUsuario = new JLabel ("Usuário> ");
-	JLabel digiteSenha = new JLabel ("Senha> ");
-	JLabel usuarioInvalido = new JLabel ();
-
+	JLabel digiteUsuario = new JLabel ("Usuário: ");
+	JLabel digiteSenha = new JLabel ("Senha: ");
+	JLabel mensagemParaUsuario = new JLabel ("Testando FONTE");
 	HashMap <String,String> informacaologin = new HashMap <String, String>();
 	
 	LoginDoSistema(HashMap <String, String> loginOriginal){
 		
 		informacaologin = loginOriginal;
+		
+		digiteUsuario.setBounds (50, 100, 75, 25);
+		digiteSenha.setBounds (50, 150, 75, 25);
+		
+		mensagemParaUsuario.setBounds(125,250,250,35);
+		mensagemParaUsuario.setFont(new Font (null, Font.ITALIC, 25));
+		
+		janelaDoPrograma.add(digiteUsuario);
+		janelaDoPrograma.add(digiteSenha);
+		janelaDoPrograma.add(mensagemParaUsuario);
+		
+		
+		janelaDoPrograma.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janelaDoPrograma.setSize (420,420);
+		janelaDoPrograma.setLayout(null);
+		janelaDoPrograma.setVisible(true);
+		
+			
 	}
 
 	@Override
